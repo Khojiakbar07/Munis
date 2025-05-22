861347b9d83148003e18e67a7b149bf9929dbf57
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-05-22 10:15:19 +0500

    modified(navbar): final navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home.jsx";
 
 function App() {
     return (
-        <div className="App">
+        <div className="app__bg">
             <Routes>
                 <Route path="/" element={<Home/>}/>
             </Routes>
